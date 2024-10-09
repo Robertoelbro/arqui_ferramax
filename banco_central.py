710227b7e8b87b4a6b36d4fae6325f9bffa80856
Robertoelbro <136745585+Robertoelbro@users.noreply.github.com>
2024-10-09 18:18:02 -0300

    Add files via upload

--- banco_central.py
+++ banco_central.py
@@ -4,31 +4,53 @@ import json
 import numpy as np
 import requests
 
+print("Ingresa los datos de tu credencial")
+correo = input("Correo: ")
+contraseña = input("Contraseña: ")
 
-siete = Siete("correo", "Contraseña")
 
-datos=siete.buscar("antofagasta")
+siete = Siete(correo, contraseña)
 
-datos=siete.cuadro(
-  series=["F032.IMC.IND.Z.Z.EP18.Z.Z.0.M", "G073.IPC.IND.2018.M"],
-  nombres = ["imacec", "ipc"],
-  desde="2010-01-01",
-  hasta="2020-12-01",
-  variacion=12,
-  frecuencia="A",
-  observado={"imacec":np.mean, "ipc":"last"}
-)
+opc = 0  
 
-print(datos)
 
-tipo_cambio = siete.cuadro(
-  series=["F072.TCO.PRE.Z.Z.M"],
-  nombres=["tipo_cambio"],
-  desde="2023-10-04",  
-  hasta="2023-10-07",  
-  frecuencia="D",  
-  observado={"tipo_cambio": "last"}  
-)
+while opc != 4:
+  print("\nTe presento la API del Banco Central")
+  print("1. Buscar sin número de serie")
+  print("2. Buscar por número de serie")
+  print("3. Mostrar métodos de 'Siete'")
+  print("4. Salir")
 
-print("Tipo de cambio de los últimos 3 días:")
-print(tipo_cambio)
+  opc = int(input("Selecciona una opción: "))
+
+  if opc == 1:
+    busqueda = input("Ingresa la serie: ")
+    
+    resultado_buscar = siete.buscar(busqueda)
+    print("Resultado de la búsqueda:")
+    print(resultado_buscar)
+
+  elif opc == 2:
+    serie = input("Ingresa tu N° de serie: ")
+    fecha_desde=input("ingresa desde que fecha(YYYY-MM-DD): ")
+    datos=siete.cuadro(
+    series=[serie],
+    nombres = ["dolar"],
+
+    desde=fecha_desde,
+    )
+    print("Datos obtenidos:")
+    print(datos)
+    
+  elif opc == 3:
+    metodos_disponibles = dir(siete)
+    metodos_publicos = [metodo for metodo in metodos_disponibles if not metodo.startswith('_')]
+    print("Métodos disponibles en la clase Siete:")
+    for metodo in metodos_publicos:
+      print(metodo)
+
+  elif opc == 4:
+    print("Gracias por su preferencia. Saliendo del programa...")
+
+  else:
+    print("Opción no válida. Intenta de nuevo.")
